85528d4b294825e1a1a280ca1e15ad68328bf264
Tommi Tervonen <tervonen@ese.eur.nl>
2011-02-18 11:06:51 +0100

    Initial webservice draft.

--- src/main/java/fi/smaa/ws/JSMAAService.java
+++ src/main/java/fi/smaa/ws/JSMAAService.java
@@ -3,14 +3,33 @@ package fi.smaa.ws;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
+import org.drugis.common.threading.ThreadHandler;
+
+import noNamespace.SMAATRIModelDocument;
 import fi.smaa.jsmaa.model.SMAATRIModel;
-import fi.smaa.jsmaa.simulator.SMAATRIResults;
+import fi.smaa.jsmaa.model.xml.xmlbeans.NonserializableModelException;
+import fi.smaa.jsmaa.model.xml.xmlbeans.XMLBeansSerializer;
+import fi.smaa.jsmaa.simulator.SMAATRISimulation;
 
 @WebService(serviceName="JSMAAService")
 public class JSMAAService {
+	
+	protected static ThreadHandler handler = ThreadHandler.getInstance();
 
-	@WebMethod(operationName="solveSMAATRIModel")
-	public SMAATRIResults solveModel(SMAATRIModel model) {
-		return null;
+	@WebMethod(operationName="solveSMAATRI")
+	synchronized public String solveSMAATRIModel(SMAATRIModelDocument model) throws NonserializableModelException {
+		XMLBeansSerializer ser = new XMLBeansSerializer();
+		
+		SMAATRIModel smaaModel = ser.deSerialize(model);
+		SMAATRISimulation simul = new SMAATRISimulation(smaaModel, 10000);
+		
+		handler.scheduleTask(simul.getTask());
+		while(handler.getRunningThreads() > 0) {
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+			}
+		}
+		return simul.getResults().toString();
 	}
 }
