cd7ba171d28835ac34a73262b86451b9b7d93584
Tommi Tervonen <tervonen@ese.eur.nl>
2011-02-22 18:08:05 +0100

    mod

--- src/main/java/fi/smaa/ws/JSMAAService.java
+++ src/main/java/fi/smaa/ws/JSMAAService.java
@@ -1,12 +1,16 @@
 package fi.smaa.ws;
 
+import java.util.List;
+import java.util.Map;
+
 import javax.jws.WebParam;
 import javax.jws.WebService;
 
+import noNamespace.AlternativeType;
 import noNamespace.SMAATRIModelDocument;
 
 @WebService
 public interface JSMAAService {
-	public String solveSMAATRIModel(
+	public Map<AlternativeType, List<Double>> solveSMAATRIModel(
 			@WebParam(name="model") SMAATRIModelDocument model);
 }
--- src/main/java/fi/smaa/ws/JSMAAServiceImpl.java
+++ src/main/java/fi/smaa/ws/JSMAAServiceImpl.java
@@ -1,15 +1,23 @@
 package fi.smaa.ws;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
 
 import org.drugis.common.threading.ThreadHandler;
 
+import noNamespace.AlternativeType;
 import noNamespace.SMAATRIModelDocument;
+import fi.smaa.jsmaa.model.Alternative;
 import fi.smaa.jsmaa.model.SMAATRIModel;
 import fi.smaa.jsmaa.model.xml.xmlbeans.NonserializableModelException;
 import fi.smaa.jsmaa.model.xml.xmlbeans.XMLBeansSerializer;
+import fi.smaa.jsmaa.simulator.SMAATRIResults;
 import fi.smaa.jsmaa.simulator.SMAATRISimulation;
 
 @WebService(serviceName="JSMAAService")
@@ -18,7 +26,7 @@ public class JSMAAServiceImpl implements JSMAAService{
 	protected static ThreadHandler handler = ThreadHandler.getInstance();
 
 	@WebMethod(operationName="solveSMAATRI")
-	synchronized public String solveSMAATRIModel(
+	synchronized public Map<AlternativeType, List<Double>> solveSMAATRIModel(
 			@WebParam(name="model")SMAATRIModelDocument model
 			) {
 		XMLBeansSerializer ser = new XMLBeansSerializer();
@@ -34,9 +42,25 @@ public class JSMAAServiceImpl implements JSMAAService{
 				} catch (InterruptedException e) {
 				}
 			}
-			return simul.getResults().toString();
+			SMAATRIResults res = simul.getResults();
+			return convertResults(res);
 		} catch (NonserializableModelException e) {
-			return "Cannot deserialize: " + e.getMessage();
+			e.printStackTrace();
+			return null;
 		}
+	}
+
+	private Map<AlternativeType, List<Double>> convertResults(SMAATRIResults res) {
+		Map<AlternativeType, List<Double>> ret = new HashMap<AlternativeType, List<Double>>();
+		for (Alternative a : res.getAlternatives()) {
+			AlternativeType at = AlternativeType.Factory.newInstance();
+			at.setName(a.getName());
+			List<Double> vals = new ArrayList<Double>();
+			for (int i=0;i<res.getCategories().size();i++) {
+				vals.add(res.getCategoryAcceptabilities().get(a).get(i));
+			}
+			ret.put(at, vals);
+		}
+		return ret;
 	}
 }
